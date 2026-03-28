91f1490dfef8cadc9f9243846b99e11e0a83b647
aimtechs2-collab <aimtechs2@gmail.com>
2026-03-28 17:33:14 +0530

    partial testing done 3

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-/** Use `proxy.ts` (Node default) instead of Edge `middleware.ts` so Clerk is not analyzed as an Edge bundle. */
 export default clerkMiddleware();
 
 export const config = {
-  // Apply Clerk to all non-static routes + API routes.
   matcher: ["/((?!.*\\..*).*)", "/", "/(api|trpc)(.*)"],
 };
